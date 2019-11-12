bb2f39dd33a8b0377d9d106e9da4f60be0b90c27
laurent stemmer <laurent.stemmer@weekendesk.fr>
2019-11-12 21:12:20 +0100

    Be able to discover 'for' patterns

--- src/main/java/com/stemlaur/romannumerals/ArabicToRoman.java
+++ src/main/java/com/stemlaur/romannumerals/ArabicToRoman.java
@@ -2,18 +2,28 @@ package com.stemlaur.romannumerals;
 
 public final class ArabicToRoman {
 
-    public String from(int arabicNumber) {
+    public String from(final int arabicNumber) {
         final StringBuilder builder = new StringBuilder();
-
-        if (arabicNumber >= 5) {
-            builder.append("V");
-            arabicNumber -= 5;
+        int remaining = arabicNumber;
+        for (int i = 100; i <= arabicNumber; i += 100) {
+            builder.append("C");
+            remaining -= 100;
         }
 
-        if (arabicNumber < 5) {
-            for (int i = 1; i <= arabicNumber; i++) {
-                builder.append("I");
-            }
+        int remainingTens = remaining;
+        for (int i = 10; i <= remainingTens; i += 10) {
+            builder.append("X");
+            remaining -= 10;
+        }
+
+        if (remaining >= 5) {
+            builder.append("V");
+            remaining -= 5;
+        }
+
+        int remainingOnes = remaining;
+        for (int i = 1; i <= remainingOnes; i++) {
+            builder.append("I");
         }
 
         return builder.toString();
--- src/test/java/com/stemlaur/romannumerals/ArabicToRomanTest.java
+++ src/test/java/com/stemlaur/romannumerals/ArabicToRomanTest.java
@@ -14,5 +14,15 @@ public class ArabicToRomanTest {
         assertEquals("V", new ArabicToRoman().from(5));
         assertEquals("VI", new ArabicToRoman().from(6));
         assertEquals("VII", new ArabicToRoman().from(7));
+        assertEquals("VIII", new ArabicToRoman().from(8));
+        assertEquals("X", new ArabicToRoman().from(10));
+        assertEquals("XI", new ArabicToRoman().from(11));
+        assertEquals("XII", new ArabicToRoman().from(12));
+        assertEquals("XIII", new ArabicToRoman().from(13));
+        assertEquals("XX", new ArabicToRoman().from(20));
+        assertEquals("XXX", new ArabicToRoman().from(30));
+        assertEquals("C", new ArabicToRoman().from(100));
+        assertEquals("CC", new ArabicToRoman().from(200));
+        assertEquals("CCC", new ArabicToRoman().from(300));
     }
 }
